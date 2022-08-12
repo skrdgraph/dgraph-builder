9dbead46f007e360fd07ff4326a539c500419163
skrdgraph <sudhish@dgraph.io>
2022-08-12 06:23:07 +0000

    fix shell script

--- dgraph-builder.sh
+++ dgraph-builder.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
 
-# check for docker
+export GOOS=$1
+export COMMIT=$2
+
+if [ $GOOS == "linux" ]
+then
+        echo "Building for 'linux'"
+        export BINARY_PATH="/root/go/bin/dgraph"
+elif [ $GOOS == "darwin" ]
+then
+        echo "Building for 'darwin'"
+        export BINARY_PATH="/root/go/bin/darwin_amd64/dgraph"
+elif [ $GOOS == "windows" ]
+then
+        echo "Building for 'windows'"
+        export BINARY_PATH="/root/go/bin/windows_amd64/dgraph"
+else
+        echo "no GOOS is entered, exiting"
+        exit 1
+fi
+
+
+if [ -z "$COMMIT" ]
+then
+      echo "no COMMIT is entered, exiting"
+      exit 1
+else
+      echo "Building for commit $COMMIT"
+fi
+
+# cleanup dgraph binary
+rm -rf /tmp/dgraph-builder/dgraph
+
+# build container base
 docker build -t dgraph-builder /Users/sudhishkr/scratch/github/skrdgraph/dgraph-builder
-docker run --name="dgraph-builder" -v /tmp/dgraph-builder:/dgraph-builder -it dgraph-builder  &>/dev/null &
+
+# run the container
+docker run --name="dgraph-builder" --env GOOS=$GOOS -v /tmp/dgraph-builder:/dgraph-builder -it dgraph-builder  &>/dev/null &
+sleep 3
+
+# exec cmd into the container
 docker exec -it $(docker ps -q --filter 'name=dgraph-builder') /bin/bash -c "git clone https://github.com/dgraph-io/dgraph.git"
-docker exec -it $(docker ps -q --filter 'name=dgraph-builder') /bin/bash -c "cd dgraph; git checkout b17395d; make install; cp /root/go/bin/dgraph /dgraph-builder/dgraph"
-docker exec -it $(docker ps -q --filter 'name=dgraph-builder') /bin/bash -c "cd dgraph; git checkout b17395d; make install; cp /root/go/bin/dgraph /dgraph-builder/dgraph"
+docker exec -it $(docker ps -q --filter 'name=dgraph-builder') /bin/bash -c "cd dgraph; git checkout $COMMIT; make install;"
+docker exec -it $(docker ps -q --filter 'name=dgraph-builder') /bin/bash -c "cp $BINARY_PATH /dgraph-builder/dgraph"
+
+# clean up
 docker stop $(docker ps -aq --filter 'name=dgraph-builder')
 docker rm $(docker ps -aq --filter 'name=dgraph-builder')
