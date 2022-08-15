1f284603702db7c0f78b78e5dee8cb6783d4a7e7
skrdgraph <sudhish@dgraph.io>
2022-08-15 06:50:16 +0000

    launcher fix

--- dgraph-launcher.sh
+++ dgraph-launcher.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-sh dgraph-builder.sh linux master
+export COMMIT=$1
+
+if [ -z "$COMMIT" ]
+then
+      echo "no COMMIT is entered, choosing master"
+      export COMMIT="master"
+else
+      echo "Building for commit $COMMIT"
+fi
+
+sh dgraph-builder.sh linux $COMMIT
 cp /tmp/dgraph-builder/dgraph ~/go/bin/dgraph
 cp hmac_secret_file /tmp/dgraph-builder/hmac_secret_file
 
